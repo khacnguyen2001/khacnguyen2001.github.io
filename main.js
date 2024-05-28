d8cba3e27aa51316f1420dbc299ab20608539cb0
khacnguyen2001 <khacnguyengithub@gmail.com>
2024-05-28 20:37:01 +0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+  
